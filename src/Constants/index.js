bce11943a8d3391522f544831994ba0ade79171e
Akramationiste <akram.chougara@gmail.com>
2023-04-20 23:52:48 +0100

    Portfolio

---
+++ src/Constants/index.js
@@ -0,0 +1,210 @@
+import { aflacLogo, cognizantLogo,
+     facebook, instagram, linkdin,
+      nodejs,
+      react, wellsLogo, wiuLogo, youtube } from "../assets"
+
+import express from "../assets/express.png"
+import bootstrap from "../assets/bootstrap.png"
+import html from "../assets/html.png"
+import css from "../assets/css.webp"
+import git from "../assets/git.png"
+import tail from "../assets/tailwind.png"
+import github from "../assets/github.png"
+import figma from "../assets/figma.png"
+import js from "../assets/javascript.png"
+import mdb from "../assets/mongodb_original_wordmark_logo_icon_146425.png"
+
+export const AppText={
+    hello:'Hello,',
+    Iam:'I am ',
+    Akram:'Akram CHOUGARA',
+    aboutMeDescripion:'I am software developer learning in IT industry from  last 6 months as Full stack developer. Expert in UI/UX design, Front-end  and Back-end Development',
+    Skills:'Skills & ',
+    Experties:'Experties',
+    UIUXDesigner:'UI/UX Designer',
+    BackendDeveloper:'Backend Dev',
+    FrontedDeveloper:'Frontend Dev',
+    Portfolio:'Portfolio',
+    Creative:'My Creative ',
+    CompaniesI:"Companies I'am ",
+    WorkedFor:'Interested In',
+    Contact:'Contact ',
+    Us:'Me',
+    copywriteText:'Copyright by Rahul Sanap @2023'
+
+}
+
+export const aboutSection=[
+    {
+        id:1,
+        image:'https://img.freepik.com/premium-vector/ui-ux-programmer-flat-design-vector-illustration-business-information-team-sharing-ideas-with-designer-coding-interface-software-app-development_2175-1809.jpg?w=2000',
+        title:'UI/UX Designer',
+        desc:'UI/UX designing software like figma'
+    },
+    {
+        id:2,
+        image:'https://media.istockphoto.com/id/1304570729/vector/front-end-development-concept-vector-flat-graphic-design-illustration.jpg?s=612x612&w=0&k=20&c=0lpu0j-4FbuoA7xpGzt9apKZeI5F9KlNNe0qRYxjUYs=',
+        title:'Frontend Developer',
+        desc:'React.JS - Html - Css - JavaScript'
+    },
+    {
+        id:3,
+        image:'https://img.freepik.com/premium-vector/back-end-developer-working-laptop_701961-1383.jpg?w=2000',
+        title:'Back-end Developer',
+        desc:'Node.JS - Express - MongoDB'
+    }
+]
+
+export const skillsList=[
+    {
+        id:1,
+        icon:js
+    },
+    {
+        id:1,
+        icon:react
+    },
+    {
+        id:1,
+        icon:html
+    },
+    {
+        id:1,
+        icon:css
+    },
+    {
+        id:1,
+        icon:mdb
+    },
+    {
+        id:1,
+        icon:bootstrap
+    },
+    {
+        id:1,
+        icon:tail
+    },
+    {
+        id:1,
+        icon:github
+    },
+    {
+        id:1,
+        icon:figma
+    },
+    {
+        id:1,
+        icon:git
+    },
+    {
+        id:1,
+        icon:express
+    },
+    {
+        id:1,
+        icon:nodejs
+    },
+
+]
+
+export const workDetail=[
+    { 
+        id:1,
+        year:'2023',
+        compnayName:'Développement web & web mobile',
+        position:'Fabrikademy'
+    },
+    { 
+        id:1,
+        year:'2022',
+        compnayName:'Master académique',
+        position:'Management E-gouvernement'
+    },
+    { 
+        id:1,
+        year:'2021',
+        compnayName:'Master académique',
+        position:'STAPS'
+    },
+    { 
+        id:1,
+        year:'2016',
+        compnayName:'Bacalauréat scientifique',
+        position:'Bacalauréat scientifique'
+    },
+]
+
+export const portfolio=[
+    {
+        id:1,
+        title:'Food Delivery UI/UX',
+        type:'ui/ux',
+        desc:"App Build using React native and compaitable for Android and IOS",
+        imageUrl:"https://cdn.dribbble.com/users/4189231/screenshots/17343305/media/202173138f9504c2eacd16e70698312c.png?compress=1&resize=400x300"
+    },
+    {
+        id:4,
+        title:'Bibliotech web site',
+        type:'website',
+        desc:"App Build using React native and compaitable for Android and IOS",
+        imageUrl:"https://miro.medium.com/max/1400/0*GWvV5kcuVATUfs2r.png"
+    },
+    {
+        id:2,
+        title:'Business Listing App',
+        type:'web App',
+        desc:"App Build using React native and compaitable for Android and IOS",
+        imageUrl:"https://www.radiustheme.com/wp-content/uploads/2022/05/Directory-app.png"
+    },
+    {
+        id:3,
+        title:'Food Delivery UI/UX',
+        type:'ui/ux',
+        desc:"App Build using React native and compaitable for Android and IOS",
+        imageUrl:"https://cdn.dribbble.com/users/2892962/screenshots/14273462/nlytical_mockup_4x.png"
+    },
+
+    
+]
+
+export const CompanyImage=[
+    {
+        id:1,
+        imageUrl:cognizantLogo
+    },
+    {
+        id:1,
+        imageUrl:wellsLogo
+    },
+    {
+        id:1,
+        imageUrl:aflacLogo
+    },
+    {
+        id:1,
+        imageUrl:wiuLogo
+    }
+]
+
+export const socialNetwork=[
+    {
+        id:1,
+        logo:instagram,
+        url:'https://www.instagram.com/tubeguruji/'
+    },
+    {
+        id:1,
+        logo:youtube,
+        url:'https://www.youtube.com/channel/UC33Tc4FCgd_to314_b2hNTg',
+    },
+    {
+        id:1,
+        logo:linkdin,
+        url:'https://www.linkedin.com/in/rahul-sanap-652920108/'
+    },
+    {
+        id:1,
+        logo:facebook,
+        url:'https://www.facebook.com/rahul.sanap1'
+    },
+]
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:  {
+        bluea: "#202A44"
+    },
+  },
+  plugins: [],
+}}
